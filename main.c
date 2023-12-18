9f890c1e4036b864ace130ab478522bd2e64ba10
henriquesati <tpedrogatinhos@gmail.com>
2023-12-18 19:01:54 -0300

    Ultimo estado com codigo antigo

--- main.c
+++ main.c
@@ -67,14 +67,14 @@ int main(int argc, char *argv[]){
     ALLEGRO_DISPLAY *display = NULL;
     ALLEGRO_EVENT_QUEUE *queue = NULL;
  	ALLEGRO_TIMER* timer = al_create_timer(1.0/FPS);
-	display = al_create_display(480, 600);
+	display = al_create_display(500, 600);
 	al_clear_to_color(al_map_rgb(255, 255, 255)); //cor de fundo
 	  al_flip_display();
 	//addons
 	al_init_primitives_addon();
 	al_install_keyboard();
 	al_init_image_addon();
-    ALLEGRO_BITMAP *snake = al_load_bitmap("snake.bmp");
+    ALLEGRO_BITMAP *snake = al_load_bitmap("snaki.bmp");
 
     queue = al_create_event_queue();
     al_register_event_source(queue, al_get_keyboard_event_source());
@@ -85,6 +85,8 @@ int main(int argc, char *argv[]){
 	int x=220;
 	int y = 300;
     node* head = init_node(x,y);
+    add(head, x-10, y);
+    add(head, x-20, y);
 	int state =83;
 	void orch2(int x, int y, ALLEGRO_COLOR cor){
 		
@@ -95,6 +97,27 @@ int main(int argc, char *argv[]){
 		
 		al_draw_rectangle(x, y, x+body, y+body, cor, 5);
 		al_flip_display();
+	}
+    void draw(node* head){
+        node *cur = head;
+        while (cur!= NULL){
+            al_draw_bitmap(snake, cur->x, cur->y, 0);
+            printf("curx: %i, cury:%i\n", cur->x, cur->y);
+            cur = cur->next;
+        }
+        show;
+    }
+    bool valid(int num){
+
+		if (state == 84 && num == 85)
+			return 0;
+		if(state == 82 && num == 83)
+			return 0;
+		if (state == 85 && num == 84)
+			return 0;
+		if(state == 83 && num == 82)
+			return 0;
+	return 1;
 	}
     while (running) {
 
@@ -109,37 +132,47 @@ int main(int argc, char *argv[]){
 			switch (event.keyboard.keycode){
 				case 84:
 				y-=inc;
-                head->y = y;
-				orch1(head->x, head->y, azul);
-                printf("headx %i -->heady %i", head->x, head->y);
-				orch1(head->x,y+inc, branco);
+                att_coords(head, x,y);
+                draw(head);
+                al_flip_display();
+                al_clear_to_color(al_map_rgb(255, 255, 255));
+				// orch1(head->x, head->y, azul);
+                // printf("headx %i -->heady %i", head->x, head->y);
+				// orch1(head->x,y+inc, branco);
 				puts("up");
 				break;
 			case 85:
 				y+=inc;
-				orch2(x, y, azul);
-				orch2(x, y-inc, branco);
+				// orch2(x, y, azul);
+				// orch2(x, y-inc, branco);
+                att_coords(head, x, y);
+                draw(head);
+                al_flip_display();
+                al_clear_to_color(al_map_rgb(255, 255, 255));
 				puts("down");
 				break;
 			case 82:
 				x-=inc;
-				orch1(x, y, azul);
-				orch1(x+inc,y, branco);
+                att_coords(head,x,y);
+                draw(head);
+                al_flip_display();
+                al_clear_to_color(al_map_rgb(255, 255, 255));              
+				// orch1(x, y, azul);
+				// orch1(x+inc,y, branco);
 				puts("left");
 				break;
 			case 83:
 				x+=inc;
 				// orch2(x, y, azul);
 				// orch2(x-inc, y, branco);
-                al_draw_bitmap(snake, x,y, 0);
-                al_draw_bitmap(snake, x,y+200, 0);
+                att_coords(head,x,y);
+                draw(head);
                 al_flip_display();
                 al_clear_to_color(al_map_rgb(255, 255, 255));
 				puts("right");
-				break;
 			
 			default:
-				printf("algo deu errado");
+				// printf("algo deu errado");
 				break;
 			}
 
